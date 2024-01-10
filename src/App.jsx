a7df8465b441639b57371c2bba268399962f5447
luis <luisnavarroFS@gmail.com>
2024-01-10 20:11:49 +0100

    routing components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import Header from './Components/Header'
+import Screen from './Components/Screen'
+import List from './Components/List'
+import Footer from './Components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Screen />
+      <List />
+      <Footer />
+    </>
+  )
+}
+
+export default App
